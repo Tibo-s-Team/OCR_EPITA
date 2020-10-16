956fef82bae9ff9d663349f2b2e242fb4e6e762c
Drevet Florian <florian.drevet@epita.fr>
2020-10-16 18:07:23 +0200

    resaux de neuronne ok. Mtn il faut l'entrainer !!

--- Python/DefNeuralNetwork.py
+++ Python/DefNeuralNetwork.py
@@ -10,6 +10,8 @@ input = [3, 4]
 def sigmoid(x):
     return 1 / (1 + math.exp(-x))
 
+def sigmoidPrime(x):
+    return x * (1 - x)
 
 def initNeuralNetwork(nbInput, hiddenLayer, nbOutPut):
     network = []
@@ -36,4 +38,39 @@ def feedfoward(network, input):
             network[i][j]["output"] = sigmoid(somme + network[i][j]["biais"])
     return network
 
-print(feedfoward(initNeuralNetwork(2, hiddenLayer, 2), input))
+def backforawrd(network, output):
+    for i in range(len(network)-1, -1, -1):
+        if i == (len(network)-1):
+            for j in range(len(network[i])):
+                network[i][j]["error"] = (output[j] - network[i][j]["output"]) * sigmoidPrime(network[i][j]["output"])
+        else:
+            for j in range(len(network[i])):
+                for k in range(len(network[i+1])):
+                    error = network[i+1][k]["weight"][j] * network[i+1][k]["error"]
+                network[i][j]["error"] = error * sigmoidPrime( network[i][j]["output"])
+    return network
+
+def updateWeight (network, inputs, coeff):
+    for i in range(len(network)):
+        for k in range(len(network[i])):
+            if i == 0:
+                for j in range(len(inputs)):
+                    network[i][k]['weight'][j] += coeff * network[i][k]['error'] * inputs[j]
+            else:
+                for j in range(len(network[i-1])):
+                    network[i][k]['weight'][j] += coeff * network[i][k]['error'] * network[i - 1][j]["output"]
+            network[i][k]['biais'] += coeff * network[i][k]['error']
+    return network
+
+def trainingNetwork(times, network):
+
+    for i in range(times):
+        network = feedfoward(network, [1, 0])
+        network = backforawrd(network, [0, 1])
+        network = updateWeight(network, input, 0.1)
+    return network
+
+##Test
+network = initNeuralNetwork(2, hiddenLayer, 2)
+trainingNetwork(20, network)
+print(feedfoward(network,[1,0]))
