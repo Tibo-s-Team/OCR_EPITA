8de4ac0deeacbb24b920aa097e482526f8908c45
Léo DUBOIN <leo.duboin@epita.fr>
2020-10-22 14:07:20 +0200

    Fallback: filtrage.c

--- src/preprocessing/preprocessing.h
+++ src/preprocessing/preprocessing.h
@@ -8,4 +8,15 @@ void grayscale(Image *image);
 void grayscaleTest(Image *image);
 void blackAndWhite(Image *image);
 
+// filtrage.c
+typedef unsigned char Uint8;
+
+typedef struct Kernel {
+    Uint8 *matrix;
+    int width, height;
+} Kernel;
+
+void convolution(Kernel *kernel);
+void printKernel(Kernel *kernel);
+
 #endif
