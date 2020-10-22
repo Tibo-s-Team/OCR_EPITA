39fcdf9ed19e5017c1aad8e236cf72b25d2430b6
Léo DUBOIN <leo.duboin@epita.fr>
2020-10-22 19:10:10 +0200

    deleted old samples

--- Python/training.py
+++ Python/training.py
@@ -57,7 +57,7 @@ resultH = [0, 0, 0, 0, 0, 0, 0, 1, 0, 0]
 resultI = [0, 0, 0, 0, 0, 0, 0, 0, 1, 0]
 resultJ = [0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
 
-listfolderLetters = creatList_folderLetters("letters\\")
+listfolderLetters = creatList_folderLetters("..\\tests\\images\\letters\\")
 exo = creatLsit_matrix(listfolderLetters)
 solution = [resultA, resultB, resultC, resultD, resultE, resultF, resultG, resultH, resultI, resultJ]
-test = creatlist_matrixtest(creatList_images("test\\"))
+test = creatlist_matrixtest(creatList_images("..\\tests\images\\test_letters\\"))
