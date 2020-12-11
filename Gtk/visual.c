c073ffb2df14717422820110a9168644974f876d
Jean Barbaroux <jean.barbaroux@epita.fr>
2020-12-11 16:09:08 +0100

    Ca marche pas snif

--- Gtk/visual.c
+++ Gtk/visual.c
@@ -1,9 +1,22 @@
 #include <gtk/gtk.h>
-#include "SDL/SDL.h"
+#include <stdio.h> 
+#include <string.h> 
+#include <SDL/SDL.h>
+#include "../src/preprocessing/preprocessing.h"
+#include "../src/preprocessing/suppression_couleurs.c"
+#include "../src/image.h"
+#include <stdlib.h>
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
+#include <err.h>
 
 typedef struct {
-    GtkWidget *w_dlg_file_choose;       // Pointer to file chooser dialog box
-    GtkWidget *w_img_main;              // Pointer to image widget
+    GtkWidget   *w_dlg_file_choose;       // Pointer to file chooser dialog box
+    GtkWidget   *button1;   
+    GtkWidget   *w_img_main;              // Pointer to image widget
+    GtkWidget   *w_img_main1;
+    GtkWidget   *label1;
+    gchar *filename;
 } app_widgets;
 
 int main(int argc, char *argv[])
@@ -19,6 +32,9 @@ int main(int argc, char *argv[])
     window = GTK_WIDGET(gtk_builder_get_object(builder, "window"));
     widgets->w_dlg_file_choose = GTK_WIDGET(gtk_builder_get_object(builder, "dlg_file_choose"));
     widgets->w_img_main = GTK_WIDGET(gtk_builder_get_object(builder, "img_main"));
+    widgets->w_img_main1 = GTK_WIDGET(gtk_builder_get_object(builder, "img_main1"));
+    widgets->button1 = GTK_WIDGET(gtk_builder_get_object(builder, "button1"));
+    widgets->label1 = GTK_WIDGET(gtk_builder_get_object(builder, "label1"));
     
     gtk_builder_connect_signals(builder, widgets);
     g_object_unref(builder);
@@ -28,7 +44,7 @@ int main(int argc, char *argv[])
     g_slice_free(app_widgets, widgets);
     
 
-    return 0;
+	return EXIT_SUCCESS;
 }
 
 // File --> Open
@@ -46,7 +62,9 @@ void on_menuitm_open_activate(GtkMenuItem *menuitem, app_widgets *app_wdgts)
         if (file_name != NULL) {
             gtk_image_set_from_file(GTK_IMAGE(app_wdgts->w_img_main), file_name);
         }
+        g_print("%s", file_name);
         g_free(file_name);
+        app_wdgts->filename = file_name;
     }
 
     // Finished with the "Open Image" dialog box, so hide it
@@ -63,4 +81,9 @@ void on_menuitm_close_activate(GtkMenuItem *menuitem, app_widgets *app_wdgts)
 void on_window_destroy()
 {
     gtk_main_quit();
+}
+
+
+void on_button1_clicked(GtkButton *b, app_widgets *app_wdgts){
+      //  char *filename = "/home/jbbx/Pictures/emoji.png";
 }
