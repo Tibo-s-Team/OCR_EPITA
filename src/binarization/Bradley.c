5bce00e14bcf45299af205d8978d844c21ba32a0
Lowen Desmarais <lowen.desmarais@epita.fr>
2020-12-12 16:17:28 +0100

    need to change to pointer implementation in Bradley and Otsu

--- src/binarization/Bradley.c
+++ src/binarization/Bradley.c
@@ -3,33 +3,29 @@
 
 #include "binarization.h"
 
-void adaptativeThreshold(Image img, unsigned int out[][], int w, int h);
+void adaptativeThreshold(Image img, int w, int h, unsigned int out[w][h]);
+//ToDo need to change everything for dynamically allocated memory arrays
 
 
 void Bradley(char *filename, char *output)
 {
     Image img = loadImage(filename);
-    unsigned int out[img.width][img.height] = {
-        {0, },
-    };
 
-    adaptativeThreshold(img, out, img.width, img.height);
+    unsigned int *arr =  malloc(w * h * sizeof(unsigned int));
+
+    adaptativeThreshold(img, img.width, img.height, out);
 
     for(int i = 0; i < img.width; i++)
     {
         for(int j = 0; j < img.height; j++)
-            setPixelColor(img, out[i][j], i, j)
+            setPixelColor(&img, out[i][j], i, j);
     }
-
-
 }
 
 
-void adaptativeThreshold(Image img, unsigned int out[][], int w, int h)
+void adaptativeThreshold(Image img, int w, int h, unsigned int out[w][h])
 {
-    unsigned long intImage[w][h] = {
-        {0, },
-    };
+    unsigned int *arr =  malloc(w * h * sizeof(unsigned int));
 
     int s = 12;
     int t = 15;
@@ -40,7 +36,7 @@ void adaptativeThreshold(Image img, unsigned int out[][], int w, int h)
 
         for(int j = 0; j < h; j++)
         {
-            int pixelValue = getPixelColor(img, i, j);
+            int pixelValue = getPixelColor(&img, i, j);
             sum += pixelValue;
 
             if (i == 0) intImage[i][j] = sum;
@@ -58,11 +54,11 @@ void adaptativeThreshold(Image img, unsigned int out[][], int w, int h)
             int y1 = (j < surrounding) ? j - surrounding : 0;
             int y2 = (j + surrounding < h) ? j+surrounding : h-1;
 
-            count = (x2-x1) * (y2-y1);
-            sum = intImage[x2][y2] - intImage[x2][y1-1] - intImage[x1-1][y2]
+            int count = (x2-x1) * (y2-y1);
+            int sum = intImage[x2][y2] - intImage[x2][y1-1] - intImage[x1-1][y2]
                 + intImage[x1-1][y1-1];
 
-            int pixel = getPixelColor(img, i, j);
+            int pixel = getPixelColor(&img, i, j);
             if (pixel * count <= sum * (100-t)/100)
                 out[i][j] = BLACK;
             else out[i][j] = WHITE;
